9ec30ea14a079fe303f2b9021ec63f65886b4321
mark <ragzouken@gmail.com>
2018-11-29 12:45:07 +0000

    fetch lost audio

--- src/download-html.ts
+++ src/download-html.ts
@@ -5,11 +5,6 @@ import {URL} from "url";
 import path from "path";
 import csv from "csv-parse/lib/sync";
 
-function findavatar(input: string): string
-{
-    return input.match(/SPR A\n([01>\n]+)\n/)![0];
-}
-
 async function scrape(browser: puppeteer.Browser,
                       url: string,
                       boid: string): Promise<void>
@@ -27,20 +22,72 @@ async function scrape(browser: puppeteer.Browser,
             filePath = `${filePath}/index.html`;
         }
 
-        await fse.outputFile(filePath, await response.buffer());
+        try
+        {
+            await fse.outputFile(filePath, await response.buffer());
+        }
+        catch (e)
+        {
+            console.log(`problem with response ${response.url()}`);
+            //console.log(e);
+        }
     });
 
-    await page.goto(url, {waitUntil: 'networkidle2'});
+    await page.goto(url, {waitUntil: 'networkidle0'});
+
+    const srcs = await page.$$eval("source", sources => sources.map(source => (source as HTMLSourceElement).src));
+
+    for (let src of srcs)
+    {
+        const url = new URL(src); 
+        const filepath = path.resolve(`./sources/${boid}${url.pathname}`);
+
+        await fse.outputFile(filepath, await fetch(src).then(r => r.buffer()));
+    }
+
     await page.close();
+}
+
+async function findGameURL(page: puppeteer.Page, url: string): Promise<string>
+{
+    await page.goto(url, {waitUntil: "networkidle2"});
+    await page.$eval(".load_iframe_btn", button => (button as HTMLButtonElement).click())
+    .catch(error => undefined);
+    
+    const iframe = await page.$eval("iframe", frame => (frame as HTMLIFrameElement).src)
+                    .catch(error => undefined);
+    
+    return iframe || url;
+}  
+
+async function scrapeGame(browser: puppeteer.Browser, url: string, boid: string)
+{
+    const page = await browser.newPage();
+
+    try
+    {
+        const game = await findGameURL(page, url);
+        
+        await scrape(browser, game, boid);
+    }
+    finally
+    {
+        await page.close();
+    }
 }
 
 (async () => {
     const response = await fetch("https://raw.githubusercontent.com/Ragzouken/bitsy-archive/master/index.txt");
     const content = await response.text();
-
     const browser = await puppeteer.launch();
-    
+
     const records = csv(content, {skip_empty_lines: true}) as string[][];
+
+    if (process.argv[2])
+    {
+        records.splice(0, records.length);
+        records.push(["TEST", "", "test", "", process.argv[2]]);
+    }
     console.log(records.length);
 
     for (let i in records)
@@ -48,28 +95,15 @@ async function scrape(browser: puppeteer.Browser,
         const line = records[i];
         const [boid, date, title, author, url, ...notes] = line;
 
-        const page = await browser.newPage();
-
         try
         {
-            await page.goto(url, {waitUntil: "networkidle2"});
-            await page.$eval(".load_iframe_btn", button => (button as HTMLButtonElement).click())
-            .catch(error => undefined);
-            
-            const iframe = await page.$eval("iframe", frame => (frame as HTMLIFrameElement).src)
-                           .catch(error => undefined);
-            
-            await scrape(browser, iframe || url, boid);
+            await scrapeGame(browser, url, boid);
         }
         catch (e)
         {
             console.log(`${i} failed ${boid} ${title} (${url})`);
             console.log(e.message);
             continue;
-        }
-        finally
-        {
-            await page.close();
         }
 
         console.log(`${i} success ${boid} ${title} (${url})`);
